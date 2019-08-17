1920335123f8c89f0962f5171de3d9df39734f1a
RicciWoo <ricciwoo@gmail.com>
2019-08-17 16:54:54 -0700

    print result

--- get_started/get_started.py
+++ get_started/get_started.py
@@ -15,4 +15,5 @@ model.compile(optimizer='adam',
 	          metrics=['accuracy'])
 
 model.fit(x_train, y_train, epochs=5)
-model.evaluate(x_test, y_test)
+result = model.evaluate(x_test, y_test)
+print(result)
