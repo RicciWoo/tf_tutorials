fff9b672ec6eea9a4cda50285a04952da09123a8
RicciWoo <ricciwoo@gmail.com>
2019-08-18 18:10:44 -0700

    update linear model

--- estimator/linear.py
+++ estimator/linear.py
@@ -7,8 +7,8 @@ import tensorflow.feature_column as fc
 import os
 import sys
 
-import matplotlib.pyplot as plt
-from IPython.display import clear_output
+# import matplotlib.pyplot as plt
+# from IPython.display import clear_output
 
 # enable eager execution to inspect this program as we run it:
 tf.enable_eager_execution()
@@ -28,20 +28,24 @@ from official.wide_deep import census_main
 
 census_dataset.download("/tmp/census_data/")
 
+#=============================================================================
+
 # Command line usage
 #export PYTHONPATH=${PYTHONPATH}:"$(pwd)/models"
 #running from python you need to set the `os.environ` or the subprocess will not see the directory.
 
-if "PYTHONPATH" in os.environ:
-  os.environ['PYTHONPATH'] += os.pathsep + models_path
-else:
-  os.environ['PYTHONPATH'] = models_path
+# if "PYTHONPATH" in os.environ:
+#   os.environ['PYTHONPATH'] += os.pathsep + models_path
+# else:
+#   os.environ['PYTHONPATH'] = models_path
 
 # Use --help to see what command line options are available:
 # !python -m official.wide_deep.census_main --help
 
 # Now run the model.
 # !python -m official.wide_deep.census_main --model_type=wide --train_epochs=2
+
+#=============================================================================
 
 # Read the U.S. Census data
 # !ls  /tmp/census_data/
@@ -57,6 +61,7 @@ test_df = pandas.read_csv(test_file, header = None, names = census_dataset._CSV_
 print(train_df.head())
 
 # Converting Data into Tensors
+# make a tf.data.Dataset by slicing the pandas.DataFrame:
 def easy_input_function(df, label_key, num_epochs, shuffle, batch_size):
   label = df[label_key]
   ds = tf.data.Dataset.from_tensor_slices((dict(df),label))
@@ -68,6 +73,7 @@ def easy_input_function(df, label_key, num_epochs, shuffle, batch_size):
 
   return ds
 
+# eager execution enabled, it's easy to inspect the resulting dataset
 ds = easy_input_function(train_df, label_key='income_bracket', num_epochs=5, shuffle=True, batch_size=10)
 
 for feature_batch, label_batch in ds.take(1):
@@ -78,6 +84,8 @@ for feature_batch, label_batch in ds.take(1):
   print('A batch of Labels:', label_batch )
 
 # Larger datasets should be streamed from disk.
+# The census_dataset.input_fn provides an example of how to 
+# do this using tf.decode_csv and tf.data.TextLineDataset:
 import inspect
 print(inspect.getsource(census_dataset.input_fn))
 
@@ -90,6 +98,25 @@ for feature_batch, label_batch in ds.take(1):
   print('Age batch   :', feature_batch['age'])
   print()
   print('Label batch :', label_batch )
+
+# wrap configurable input function
+import functools
+
+train_inpf = functools.partial(census_dataset.input_fn, train_file, num_epochs=2, shuffle=True, batch_size=64)
+test_inpf = functools.partial(census_dataset.input_fn, test_file, num_epochs=1, shuffle=False, batch_size=64)
+
+# Selecting and Engineering Features for the Model
+# Base Function Columns
+# Numeric columns
+age = fc.numeric_column('age')
+print(fc.input_layer(feature_batch, [age]).numpy())
+
+
+
+
+
+
+
 
 
 
