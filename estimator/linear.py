6a1c00ead6e7ef4482d41de4edd9f0d6d2ff3f56
RicciWoo <ricciwoo@gmail.com>
2019-08-24 12:30:16 -0700

    update linear model

--- estimator/linear.py
+++ estimator/linear.py
@@ -7,8 +7,8 @@ import tensorflow.feature_column as fc
 import os
 import sys
 
-import matplotlib.pyplot as plt
-from IPython.display import clear_output
+# import matplotlib.pyplot as plt
+# from IPython.display import clear_output
 
 # enable eager execution to inspect this program as we run it:
 tf.enable_eager_execution()
@@ -55,8 +55,8 @@ test_file = "/tmp/census_data/adult.test"
 
 import pandas
 
-train_df = pandas.read_csv(train_file, header = None, names = census_dataset._CSV_COLUMNS)
-test_df = pandas.read_csv(test_file, header = None, names = census_dataset._CSV_COLUMNS)
+train_df = pandas.read_csv(train_file, header=None, names=census_dataset._CSV_COLUMNS)
+test_df = pandas.read_csv(test_file, header=None, names=census_dataset._CSV_COLUMNS)
 
 print(train_df.head())
 
@@ -64,7 +64,7 @@ print(train_df.head())
 # make a tf.data.Dataset by slicing the pandas.DataFrame:
 def easy_input_function(df, label_key, num_epochs, shuffle, batch_size):
   label = df[label_key]
-  ds = tf.data.Dataset.from_tensor_slices((dict(df),label))
+  ds = tf.data.Dataset.from_tensor_slices((dict(df), label))
 
   if shuffle:
     ds = ds.shuffle(10000)
@@ -81,13 +81,13 @@ for feature_batch, label_batch in ds.take(1):
   print()
   print('A batch of Ages  :', feature_batch['age'])
   print()
-  print('A batch of Labels:', label_batch )
+  print('A batch of Labels:', label_batch)
 
 # Larger datasets should be streamed from disk.
 # The census_dataset.input_fn provides an example of how to 
 # do this using tf.decode_csv and tf.data.TextLineDataset:
-import inspect
-print(inspect.getsource(census_dataset.input_fn))
+# import inspect
+# print(inspect.getsource(census_dataset.input_fn))
 
 # This input_fn returns equivalent output:
 ds = census_dataset.input_fn(train_file, num_epochs=5, shuffle=True, batch_size=10)
@@ -97,7 +97,7 @@ for feature_batch, label_batch in ds.take(1):
   print()
   print('Age batch   :', feature_batch['age'])
   print()
-  print('Label batch :', label_batch )
+  print('Label batch :', label_batch)
 
 # wrap configurable input function
 import functools
@@ -116,7 +116,7 @@ classifier = tf.estimator.LinearClassifier(feature_columns=[age])
 classifier.train(train_inpf)
 result = classifier.evaluate(test_inpf)
 
-clear_output()  # used for display in notebook
+# clear_output()  # used for display in notebook
 print(result)
 
 # define a NumericColumn for each continuous feature column
@@ -125,39 +125,34 @@ capital_gain = tf.feature_column.numeric_column('capital_gain')
 capital_loss = tf.feature_column.numeric_column('capital_loss')
 hours_per_week = tf.feature_column.numeric_column('hours_per_week')
 
-my_numeric_columns = [age,education_num, capital_gain, capital_loss, hours_per_week]
-
+my_numeric_columns = [age, education_num, capital_gain, capital_loss, hours_per_week]
 print(fc.input_layer(feature_batch, my_numeric_columns).numpy())
 
 # retrain a model on these features by changing 
 # the feature_columns argument to the constructor:
 classifier = tf.estimator.LinearClassifier(feature_columns=my_numeric_columns)
 classifier.train(train_inpf)
-
 result = classifier.evaluate(test_inpf)
 
-clear_output()
-
+# clear_output()
 for key, value in sorted(result.items()):
   print('%s: %s' % (key, value))
 
 # Categorical columns
+# if know the set of all possible feature values of the column
 relationship = fc.categorical_column_with_vocabulary_list(
     'relationship',
     ['Husband', 'Not-in-family', 'Wife', 'Own-child', 'Unmarried', 'Other-relative'])
-
 print(fc.input_layer(feature_batch, [age, fc.indicator_column(relationship)]))
 
+# if don't know the set of possible values in advance
 occupation = tf.feature_column.categorical_column_with_hash_bucket(
     'occupation', hash_bucket_size=1000)
-
 for item in feature_batch['occupation'].numpy():
     print(item.decode())
 
 occupation_result = fc.input_layer(feature_batch, [fc.indicator_column(occupation)])
-
 print(occupation_result.numpy().shape)
-
 print(tf.argmax(occupation_result, axis=1).numpy())
 
 education = tf.feature_column.categorical_column_with_vocabulary_list(
@@ -183,8 +178,7 @@ classifier = tf.estimator.LinearClassifier(feature_columns=my_numeric_columns+my
 classifier.train(train_inpf)
 result = classifier.evaluate(test_inpf)
 
-clear_output()
-
+# clear_output()
 for key, value in sorted(result.items()):
   print('%s: %s' % (key, value))
 
@@ -192,7 +186,6 @@ for key, value in sorted(result.items()):
 # Make Continuous Features Categorical through Bucketization
 age_buckets = tf.feature_column.bucketized_column(
     age, boundaries=[18, 25, 30, 35, 40, 45, 50, 55, 60, 65])
-
 print(fc.input_layer(feature_batch, [age, age_buckets]).numpy())
 
 # Learn complex relationships with crossed column
@@ -228,12 +221,10 @@ train_inpf = functools.partial(census_dataset.input_fn, train_file,
 
 model.train(train_inpf)
 
-clear_output()  # used for notebook display
-
+# clear_output()  # used for notebook display
 results = model.evaluate(test_inpf)
 
-clear_output()
-
+# clear_output()
 for key, value in sorted(results.items()):
   print('%s: %0.2f' % (key, value))
 
@@ -255,8 +246,7 @@ for pred_dict in pred_iter:
 predict_df['predicted_class'] = classes[np.array(pred_class_id)]
 predict_df['correct'] = predict_df['predicted_class'] == predict_df['income_bracket']
 
-clear_output()
-
+# clear_output()
 print(predict_df[['income_bracket','predicted_class', 'correct']])
 
 # Adding Regularization to Prevent Overfitting
@@ -288,6 +278,8 @@ clear_output()
 for key in sorted(results):
   print('%s: %0.2f' % (key, results[key]))
 
+# Look at the model's weight distribution to 
+# better see the effect of the regularization
 def get_flat_weights(model):
   weight_names = [
       name for name in model.get_variable_names()
@@ -303,6 +295,25 @@ weights_flat = get_flat_weights(model)
 weights_flat_l1 = get_flat_weights(model_l1)
 weights_flat_l2 = get_flat_weights(model_l2)
 
+# mask zero-valued weights when viewing the weight distributions
+weight_mask = weights_flat != 0
 
+weights_base = weights_flat[weight_mask]
+weights_l1 = weights_flat_l1[weight_mask]
+weights_l2 = weights_flat_l2[weight_mask]
 
+# Now plot the distributions:
+# plt.figure()
+# _ = plt.hist(weights_base, bins=np.linspace(-3,3,30))
+# plt.title('Base Model')
+# plt.ylim([0,500])
 
+# plt.figure()
+# _ = plt.hist(weights_l1, bins=np.linspace(-3,3,30))
+# plt.title('L1 - Regularization')
+# plt.ylim([0,500])
+
+# plt.figure()
+# _ = plt.hist(weights_l2, bins=np.linspace(-3,3,30))
+# plt.title('L2 - Regularization')
+# _=plt.ylim([0,500])
